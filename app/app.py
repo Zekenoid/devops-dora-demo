bc851b56272e161496b4cc05fc587b85f10f3ba5
Harshit Dobriyal <harshitdobriyal6@gmail.com>
2025-11-10 13:12:24 +0530

    error app.py

--- app/app.py
+++ app/app.py
@@ -1,1 +1,19 @@
-"pri('HelevOps!
+Step 3: Workflow Implementation
+
+Every time you commit, GitHub Actions:
+
+Runs your build & test stages.
+
+Deploys automatically if tests pass.
+
+To simulate real-world scenarios:
+
+Introduce a syntax error in your code → commit → observe pipeline failure.
+
+Fix the issue → commit again → observe successful recovery.
+
+This allows you to collect data for:
+
+Failed deployments (for CFR)
+
+Recovery times (for MTTR)
